f4c50f2e2a912cc7ed673470bf53d1f23444a4b2
aryan eftekhari <aryan.eftekhari@gmail.com>
2014-07-14 22:22:14 -0700

    update

--- install.sh
+++ install.sh
@@ -1,26 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+
 echo "Nicedoor Installtion ..."
 echo $1
 
-if [[ -z "$1" ]]; then
-
-	echo ">> install.sh etc/init.d/<PATH-TO-RUN-SCRIPT-USING-DASHES-NOT-SLASHES@USERNAME.sh>"
-	echo '\n'
-
+if [ -z "$1" ]; then
+  echo ">> install.sh etc/init.d/<PATH-TO-RUN-SCRIPT-USING-DASHES-NOT-SLASHES@USERNAME.sh>"
+  echo '\n'
 else
-
-	if [[ "$1"==$(basename $1) ]]; then
-		PATH = "/etc/init.d/"$1
-	else
-		PATH = $1
-	fi
-	
-	echo "\n"
-	echo "Installed In: "$PATH
-	echo "\n"
-
-	sudo cp nicedoor.sh $PATH
-	sudo chmod 755 $PATH
-	NAME=$(basename $1)
-	sudo update-rc.d $NAME defaults
+  if [ "$1"==$(basename $1) ]; then
+    PATH="/etc/init.d/"$1
+  else
+    PATH=$1
+  fi
+  
+  echo "\n"
+  echo "Installed In: "$PATH
+  echo "\n"
+  sudo cp nicedoor.sh $PATH
+  sudo chmod 755 $PATH
+  NAME=$(basename $1)
+  sudo update-rc.d $NAME defaults
 fi
