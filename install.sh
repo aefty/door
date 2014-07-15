1472936f9122b9a86339408d433da1d908304306
aryan eftekhari <aryan.eftekhari@gmail.com>
2014-07-14 20:48:49 -0700

    update

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ if [[ -z "$1" ]]; then
 	echo ">> install.sh <PATH-TO-RUN-SCRIPT-USING-DASHES-NOT-SLASHES@USERNAME.sh>"
 	echo '\n'
 else
-	sudo cp ./nicedoor.sh /etc/init.d/$1
-	sudo chmod 755 /etc/init.d/$1
-	sudo update-rc.d $1 defaults
+	sudo cp ./nicedoor.sh $1
+	sudo chmod 755 $1
+	NAME=$(basename $1)
+
+	sudo update-rc.d $NAME defaults
 fi
